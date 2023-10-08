c1199daf2cd5955d848910785e723d9192b6824c
syafrinaaini <137477786+syafrinaaini@users.noreply.github.com>
2023-10-08 10:06:20 +0700

    Create produk_form.dart

---
+++ produk_form.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/ui/produk_detail.dart'; // Sesuaikan dengan path yang benar ke file ProdukDetail
+
+class ProdukForm extends StatefulWidget {
+  const ProdukForm({Key? key}) : super(key: key);
+
+  @override
+  _ProdukFormState createState() => _ProdukFormState();
+}
+
+class _ProdukFormState extends State<ProdukForm> {
+  final _kodeProdukTextboxController = TextEditingController();
+  final _namaProdukTextboxController = TextEditingController();
+  final _hargaProdukTextboxController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Form Produk'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _textboxKodeProduk(),
+            _textboxNamaProduk(),
+            _textboxHargaProduk(),
+            _tombolSimpan(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _textboxKodeProduk() {
+    return TextField(
+      decoration: const InputDecoration(
+          labelText: "Kode Produk",
+          labelStyle: TextStyle(color: Color(0xff0d5d83)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.black))),
+      controller: _kodeProdukTextboxController,
+    );
+  }
+
+  Widget _textboxNamaProduk() {
+    return TextField(
+      decoration: const InputDecoration(
+          labelText: "Nama Produk",
+          labelStyle: TextStyle(color: Color(0xff0d5d83)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.black))),
+      controller: _namaProdukTextboxController,
+    );
+  }
+
+  Widget _textboxHargaProduk() {
+    return TextField(
+      decoration: const InputDecoration(
+          labelText: "Harga",
+          labelStyle: TextStyle(color: Color(0xff0d5d83)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.black))),
+      controller: _hargaProdukTextboxController,
+    );
+  }
+
+  Widget _tombolSimpan() {
+    return ElevatedButton(
+      onPressed: () {
+        String kodeProduk = _kodeProdukTextboxController.text;
+        String namaProduk = _namaProdukTextboxController.text;
+        int harga = int.parse(_hargaProdukTextboxController.text);
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => ProdukDetail(
+            kodeProduk: kodeProduk,
+            namaProduk: namaProduk,
+            harga: harga,
+          ),
+        ));
+      },
+      child: const Text('Simpan'),
+    );
+  }
+}
